5395d318586b008d778aa44c063eede8b90d98c3
noahhhx <mrnoahgiles@gmail.com>
2026-08-29 09:56:49 +0100

    Update configuration.nix

--- hosts/fw13/configuration.nix
+++ hosts/fw13/configuration.nix
@@ -6,7 +6,7 @@
     ];
 
     boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.efi.canTouchEfiVariables = false;
 
     networking.hostName = "fw13";
     networking.networkmanager.enable = true;
