6c8c8202a494d3376042ceb20f788128025680f3
noahhhx <mrnoahgiles@gmail.com>
2026-08-31 08:35:26 +0100

    Update hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -17,9 +17,9 @@
       settings = {
         monitor = [ ",preferred,auto,1" ];
         bind = [
-          "SUPER, Q, exec, ghostty"    # the terminal feature provides ghostty
-          "SUPER, F, fullscreen, 0"
-          "SUPER, M, exit"
+          "SUPER + Q, exec, ghostty"    # the terminal feature provides ghostty
+          "SUPER + F, fullscreen, 0"
+          "SUPER + M, exit"
         ];
         bindm = [
           "SUPER, mouse:272, movewindow"
